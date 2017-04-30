e86ad448d69ad2d1cd1bee6d2c6b054253b59a19
Chia-Yu Tsai <Chia-Yu Tsai>
2017-04-30 08:09:39 +0800

    jobType.js 1. rework the raw queries into standard knex query 2. redone order adjustment route logic (prevent adjusting order to a value outside of range)

--- src/backend/route/data/jobType.js
+++ src/backend/route/data/jobType.js
@@ -35,7 +35,9 @@ router.route('/data/jobTypes/id/:id')
     .get((request, response, next) => { // get a particular record
         let id = request.params.id;
         let knex = require('knex')(mssqlConfig);
-        knex('scheduleSystem.dbo.jobType').select('*').where({ id: id }).debug(false)
+        knex('scheduleSystem.dbo.jobType')
+            .select('*')
+            .where({ id: id }).debug(false)
             .then((resultset) => {
                 return response.status(200).json(resultset);
             }).catch((error) => {
@@ -62,7 +64,9 @@ router.route('/data/jobTypes/id/:id')
             );
         } else {
             let knex = require('knex')(mssqlConfig);
-            knex('scheduleSystem.dbo.jobType').update({ reference: reference }).where({ id: id }).debug(false)
+            knex('scheduleSystem.dbo.jobType')
+                .update({ reference: reference })
+                .where({ id: id }).debug(false)
                 .then(() => {
                     return response.status(204).end();
                 }).catch((error) => {
@@ -83,18 +87,26 @@ router.route('/data/jobTypes/id/:id')
         let knex = require('knex')(mssqlConfig);
         knex.transaction((trx) => {
             // get target record's current data
-            return trx('scheduleSystem.dbo.jobType').select('displaySequence').where({ id: id }).debug(false)
+            return trx('scheduleSystem.dbo.jobType')
+                .select('displaySequence')
+                .where({ id: id }).debug(false)
                 .then((resultset) => {
                     targetRecordDisplaySequence = resultset[0].displaySequence;
                     // deactivate the target record
-                    return trx('scheduleSystem.dbo.jobType').update({
-                        displaySequence: null,
-                        active: 0
-                    }).where({ id: id }).debug(false);
+                    return trx('scheduleSystem.dbo.jobType')
+                        .update({
+                            displaySequence: null,
+                            active: 0
+                        }).where({ id: id }).debug(false);
                 }).then(() => {
                     // update all active records that are preceeded by the target record to displaySequence -1
-                    // note: raw query was used due to an issue of getting standard knex query to work
-                    return trx.raw('UPDATE scheduleSystem.dbo.jobType SET displaySequence=displaySequence-1 WHERE active=1 AND displaySequence>?;', [targetRecordDisplaySequence]).debug(false);
+                    return trx('scheduleSystem.dbo.jobType')
+                        .decrement('displaySequence', 1)
+                        .where({
+                            active: 1,
+                            displaySequence: targetRecordDisplaySequence
+                        })
+                        .debug(false);
                 }).then(() => { // get a fresh set of jobType data
                     return trx('scheduleSystem.dbo.jobType')
                         .select('*').where({ active: 1, deprecated: null })
@@ -119,36 +131,64 @@ router.route('/data/jobTypes/id/:id/displaySequence/:displaySequence')
     .patch((request, response, next) => { // reorder active record list
         let id = request.params.id;
         let targetRecordDisplaySequence = null;
+        let upperLimit = null;
         let knex = require('knex')(mssqlConfig);
         knex.transaction((trx) => {
             // get target record's current displaySequence value
-            return trx('scheduleSystem.dbo.jobType').select('displaySequence').where({ id: id }).debug(false)
+            return trx('scheduleSystem.dbo.jobType')
+                .select('displaySequence')
+                .where({ id: id }).debug(false)
                 .then((resultset) => {
                     targetRecordDisplaySequence = resultset[0].displaySequence;
-                    // check the original and final displaySequence to determine how to reorder
-                    if (targetRecordDisplaySequence < request.params.displaySequence) {
-                        // adjust displaySequence of all affected active records
-                        // note: raw query was used due to an issue of getting standard knex query to work
-                        return trx.raw('UPDATE scheduleSystem.dbo.jobType SET displaySequence=displaySequence-1 WHERE active=1 AND displaySequence>? AND displaySequence<=?;', [targetRecordDisplaySequence, request.params.displaySequence]).debug(false);
-                    } else if (targetRecordDisplaySequence > request.params.displaySequence) {
-                        // adjust displaySequence of all affected active records
-                        // note: raw query was used due to an issue of getting standard knex query to work
-                        return trx.raw('UPDATE scheduleSystem.dbo.jobType SET displaySequence=displaySequence+1 WHERE active=1 AND displaySequence>=? AND displaySequence<?;', [request.params.displaySequence, targetRecordDisplaySequence]).debug(false);
-                    } else { // original value and target value are the same
+                    // get the current highest displaySequence value
+                    return trx('scheduleSystem.dbo.jobType')
+                        .max('displaySequence as maxDisplaySequence').debug(false);
+                }).then((resultset) => {
+                    upperLimit = resultset[0].maxDisplaySequence;
+                    if (
+                        // check if the intended sequence value is out of range
+                        (upperLimit < request.params.displaySequence) ||
+                        // check if the target record is already at the intended sequence value
+                        (targetRecordDisplaySequence === request.params.displaySequence)
+                    ) { // skip operation
                         return Promise.resolve();
+                    } else { // check the original and final displaySequence to determine how to reorder
+                        if (targetRecordDisplaySequence < request.params.displaySequence) {
+                            // adjust displaySequence of all affected active records
+                            return trx('scheduleSystem.dbo.jobType')
+                                .decrement('displaySequence', 1)
+                                .where({ active: 1 })
+                                .where('displaySequence', '>', targetRecordDisplaySequence)
+                                .where('displaySequence', '<=', request.params.displaySequence)
+                                .debug(false);
+                        } else { // adjust displaySequence of all affected active records
+                            return trx('scheduleSystem.dbo.jobType')
+                                .increment('displaySequence', 1)
+                                .where({ active: 1 })
+                                .where('displaySequence', '>=', request.params.displaySequence)
+                                .where('displaySequence', '<', targetRecordDisplaySequence)
+                                .debug(false);
+                        }
                     }
-                }).then(() => { // update the target with intended displaySequence
-                    if (targetRecordDisplaySequence !== request.params.displaySequenc) {
+                }).then(() => {
+                    if (
+                        // check if the intended sequence value is out of range
+                        (upperLimit < request.params.displaySequence) ||
+                        // check if the target record is already at the intended sequence value
+                        (targetRecordDisplaySequence === request.params.displaySequence)
+                    ) { // skip operation
+                        return Promise.resolve();
+                    } else { // update the target with intended displaySequence
                         return trx('scheduleSystem.dbo.jobType')
                             .update({ displaySequence: request.params.displaySequence })
                             .where({ id: id }).debug(false);
-                    } else {
-                        return Promise.resolve();
                     }
                 }).then(() => { // get a fresh set of jobType data
                     return trx('scheduleSystem.dbo.jobType')
-                        .select('*').where({ active: 1, deprecated: null })
-                        .orderBy('displaySequence').debug(false);
+                        .select('*')
+                        .where({ active: 1, deprecated: null })
+                        // .orderBy('displaySequence')
+                        .debug(false);
                 });
         }).then((resultset) => {
             return response.status(200).json(resultset);
@@ -169,7 +209,8 @@ router.route('/data/jobTypes')
     .get((request, response, next) => { // get all active records
         let knex = require('knex')(mssqlConfig);
         knex('scheduleSystem.dbo.jobType')
-            .select('*').where({ active: 1, deprecated: null })
+            .select('*')
+            .where({ active: 1, deprecated: null })
             .orderBy('displaySequence').debug(false)
             .then((resultset) => {
                 return response.status(200).json(resultset);
@@ -187,6 +228,7 @@ router.route('/data/jobTypes')
     .post((request, response, next) => { // insert a new active record to the end of the list
         let knex = require('knex')(mssqlConfig);
         knex.transaction((trx) => {
+            // get the current highest displaySequence value
             return trx('scheduleSystem.dbo.jobType').max('displaySequence as maxDisplaySequence').debug(false)
                 .then((resultset) => {
                     let recordData = {
@@ -230,7 +272,9 @@ router.route('/data/jobTypes/inactive')
     .get((request, response, next) => { // get all inactive but not deprecated records
         let id = request.params.id;
         let knex = require('knex')(mssqlConfig);
-        knex('scheduleSystem.dbo.jobType').select('*').where({ active: 0, deprecated: null }).debug(false)
+        knex('scheduleSystem.dbo.jobType')
+            .select('*')
+            .where({ active: 0, deprecated: null }).debug(false)
             .then((resultset) => {
                 return response.status(200).json(resultset);
             }).catch((error) => {
@@ -238,7 +282,7 @@ router.route('/data/jobTypes/inactive')
                     endpointErrorHandler(
                         request.method,
                         request.originalUrl,
-                        `工作類別資料 [${id}] 讀取發生錯誤: ${error}`)
+                        `工作類別資料(停用中) [${id}] 讀取發生錯誤: ${error}`)
                 );
             }).finally(() => {
                 knex.destroy();
@@ -252,16 +296,18 @@ router.route('/data/jobTypes/inactive/id/:id')
         let knex = require('knex')(mssqlConfig);
         knex.transaction((trx) => {
             // get the current highest displaySequence value
-            return trx('scheduleSystem.dbo.jobType').max('displaySequence as maxDisplaySequence').debug(false)
+            return trx('scheduleSystem.dbo.jobType')
+                .max('displaySequence as maxDisplaySequence').debug(false)
                 .then((resultset) => {
-                    return trx('scheduleSystem.dbo.jobType').update({
-                        displaySequence: resultset[0].maxDisplaySequence === null ? 0 : resultset[0].maxDisplaySequence + 1,
-                        active: 1,
-                        deprecated: null
-                    }).where({
-                        id: id,
-                        active: 0
-                    }).returning(['id', 'reference', 'displaySequence', 'active', 'deprecated']).debug(false);
+                    return trx('scheduleSystem.dbo.jobType')
+                        .update({
+                            displaySequence: resultset[0].maxDisplaySequence === null ? 0 : resultset[0].maxDisplaySequence + 1,
+                            active: 1,
+                            deprecated: null
+                        }).where({
+                            id: id,
+                            active: 0
+                        }).returning(['id', 'reference', 'displaySequence', 'active', 'deprecated']).debug(false);
                 });
         }).then((resultset) => {
             return response.status(200).json(resultset);
@@ -284,8 +330,7 @@ router.route('/data/jobTypes/inactive/id/:id')
         };
         let knex = require('knex')(mssqlConfig);
         knex.transaction((trx) => {
-            // deprecate the target record
-            return trx('scheduleSystem.dbo.jobType')
+            return trx('scheduleSystem.dbo.jobType') // deprecate the target record
                 .update(standardDeprecatedData)
                 .where({
                     id: id,
@@ -305,13 +350,14 @@ router.route('/data/jobTypes/inactive/id/:id')
         });
     });
 
-router.route('/data/jobTypes/all')
+router.route('/data/jobTypes/deprecated')
     .all(tokenValidation)
-    .get((request, response, next) => { // get all records
+    .get((request, response, next) => { // get all deprecated records
         let id = request.params.id;
         let knex = require('knex')(mssqlConfig);
-        knex('scheduleSystem.dbo.jobType').select('*')
-            .orderBy('deprecated').orderBy('active', 'desc').orderBy('displaySequence').debug(false)
+        knex('scheduleSystem.dbo.jobType')
+            .select('*')
+            .whereNotNull('deprecated').debug(false)
             .then((resultset) => {
                 return response.status(200).json(resultset);
             }).catch((error) => {
@@ -319,7 +365,30 @@ router.route('/data/jobTypes/all')
                     endpointErrorHandler(
                         request.method,
                         request.originalUrl,
-                        `工作類別資料 [${id}] 讀取發生錯誤: ${error}`)
+                        `工作類別資料(永遠停用項目) [${id}] 讀取發生錯誤: ${error}`)
+                );
+            }).finally(() => {
+                knex.destroy();
+            });
+    });
+
+router.route('/data/jobTypes/all')
+    .all(tokenValidation)
+    .get((request, response, next) => { // get all records
+        let id = request.params.id;
+        let knex = require('knex')(mssqlConfig);
+        knex('scheduleSystem.dbo.jobType').select('*')
+            .orderBy('deprecated')
+            .orderBy('active', 'desc')
+            .orderBy('displaySequence').debug(false)
+            .then((resultset) => {
+                return response.status(200).json(resultset);
+            }).catch((error) => {
+                return response.status(500).json(
+                    endpointErrorHandler(
+                        request.method,
+                        request.originalUrl,
+                        `工作類別資料(所有資料) [${id}] 讀取發生錯誤: ${error}`)
                 );
             }).finally(() => {
                 knex.destroy();
