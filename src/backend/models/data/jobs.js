bd1e407a33c2d24c60ba0810d0ff0b7294254e21
Chia-Yu Tsai <Chia-Yu Tsai>
2017-05-15 20:34:18 +0800

    models
    updated missing jobs model definition

--- src/backend/models/data/jobs.js
+++ src/backend/models/data/jobs.js
@@ -1,52 +1,72 @@
-import { Model } from '../config/database.js';
-import { Combined } from './clients.js';
-import JobTypes from './jobTypes.js';
+import Sequelize from 'sequelize';
+import { sequelize } from '../../config/database.js';
 
-export default class Jobs extends Model {
-    static tableName = 'scheduleSystem.dbo.jobs';
-    static idColumn = 'id';
-    static jsonSchema = {
-        type: 'object',
-        required: [
-            'jobTypeId',
-            'reference',
-            'clientId',
-            'deadline',
-            'createdBy',
-            'updatedBy'
-        ],
-        properties: {
-            id: { type: 'string' },
-            jobTypeId: { type: 'string' },
-            reference: { type: 'string' },
-            clientId: { type: 'string' },
-            description: { type: ['string', null] },
-            deadline: { type: 'string' },
-            observable: { type: 'boolean' },
-            createdAt: { type: 'string' },
-            createdBy: { type: 'string' },
-            updatedAt: { type: 'string' },
-            updatedBy: { type: 'string' },
-            deletedAt: { type: ['string', null] },
-            deletedBy: { type: ['string', null] }
-        }
-    };
-    static relationMappings = {
-        clientDetail: {
-            relation: Model.BelongsToOneRelation,
-            modelClass: Combined,
-            join: {
-                from: 'scheduleSystem.dbo.jobs.clientId',
-                to: 'scheduleSystem.dbo.clients.CUS_NO'
-            }
-        },
-        jobType: {
-            relation: Model.BelongsToOneRelation,
-            modelClass: JobTypes,
-            join: {
-                from: 'scheduleSystem.dbo.jobs.jobTypeId',
-                to: 'scheduleSystem.dbo.jobTypes.id'
-            }
-        }
-    };
-}
+export const Jobs = sequelize.define('jobs', {
+    id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        validate: { isUUID: 4 }
+    },
+    jobTypeId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    reference: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    clientId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: { isUUID: 4 }
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: true
+    },
+    deadline: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        validate: { isDate: true }
+    },
+    observable: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+    },
+    createdBy: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: { isUUID: 4 }
+    },
+    updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+    },
+    updatedBy: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: { isUUID: 4 }
+    },
+    deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true
+    },
+    deletedBy: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        validate: { isUUID: 4 }
+    }
+}, {
+    createdAt: 'createdAt',
+    updatedAt: 'updatedAt',
+    deletedAt: 'deletedAt'
+});
